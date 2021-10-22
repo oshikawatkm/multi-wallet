3afd224acb903dbea9f3fd8b9f40f477082e49bd
oshikawatkm <tkm05oshikawa12@gmail.com>
2021-10-22 14:28:49 +0900

    modify return

--- did_methods/hlindy/_hlindyDid.js
+++ did_methods/hlindy/_hlindyDid.js
@@ -36,7 +36,7 @@ class HLindyDidObject {
   async getDid() {
     let wallet = new Wallet(this.agent);
     let dids = await wallet.didPublic();
-    return dids.results[0].did;
+    return dids.result.did;
   }
 
   async getSchemaId(agent, schemaName){
